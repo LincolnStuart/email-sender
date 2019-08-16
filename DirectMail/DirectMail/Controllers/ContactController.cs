92228caa0a8af2b150fdf90481fb435075a87ea7
LincolnStuart <lincoln.stuartlg@gmail.com>
2019-08-16 10:42:40 -0300

    Initial

---
+++ DirectMail/DirectMail/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DirectMail.Helpers;
+using DirectMail.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DirectMail.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly EmailHelper emailHelper;
+
+        public ContactController(EmailHelper emailHelper)
+        {
+            this.emailHelper = emailHelper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(Message model)
+        {
+            if (ModelState.IsValid)
+            {
+                var task = Task.Factory.StartNew(async () => await emailHelper.SendMail(model));
+                Task.WaitAll(task);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DirectMail/DirectMail/Helpers/EmailHelper.cs
@@ -0,0 +1,30 @@
+﻿using DirectMail.Models;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace DirectMail.Helpers
+{
+    public class EmailHelper
+    {
+        private readonly SmtpClient smtpClient;
+        private readonly string mainMail;
+
+        public EmailHelper(SmtpClient smtpClient, string mainMail)
+        {
+            this.smtpClient = smtpClient;
+            this.mainMail = mainMail;
+        }
+
+        public async Task SendMail(Message model)
+        {
+            using (var message = new MailMessage(mainMail, mainMail)
+            {
+                Subject = model.Subject,
+                Body = $"Nome: {model.FirstName} {model.LastName}\r\nE-mail: {model.Email}\r\n\r\n{model.Description}"
+            })
+            {
+                await smtpClient.SendMailAsync(message);
+            }
+        }
+    }
+    }
